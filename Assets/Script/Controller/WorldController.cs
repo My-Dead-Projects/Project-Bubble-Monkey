defd2325c8ad66637b66db3d8cc71e1426e195fe
Michael Dorst <code@mikefdorst.com>
2017-01-01 23:47:43 -0800

    Initial Commit

---
+++ Assets/Script/Controller/WorldController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldController : MonoBehaviour {
+
+	World world;
+	public int world_width;
+	public int world_height;
+	public Sprite sprite;
+
+	// Use this for initialization
+	void Start () {
+		world = new World (world_width, world_height);
+
+		for (int x = 0; x < world.Width; x++) {
+			for (int y = 0; y < world.Height; y++) {
+				Tile tile = world.GetTileAt (x, y);
+				GameObject tile_object = new GameObject ();
+				tile_object.name = "Tile_" + x + "_" + y;
+				tile_object.transform.position = new Vector3 (tile.X, tile.Y, 0);
+				SpriteRenderer tile_sprite = tile_object.AddComponent<SpriteRenderer> ();
+				tile_sprite.sprite = sprite;
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Script/Model/Tile.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+
+public class Tile {
+
+	World world;
+	int x;
+	public int X {
+		get {
+			return x;
+		}
+	}
+	int y;
+	public int Y {
+		get {
+			return y;
+		}
+	}
+
+	public Tile(World world, int x, int y) {
+		this.world = world;
+		this.x = x;
+		this.y = y;
+	}
+}
+
