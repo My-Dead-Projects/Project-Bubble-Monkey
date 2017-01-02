3f76390f92dfd825cd96600d9d109df5525f4b62
Michael Dorst <code@mikefdorst.com>
2017-01-02 04:50:48 -0800

    Whatever I did

--- Assets/Editor/Tools.cs
+++ Assets/Editor/Tools.cs
@@ -3,20 +3,13 @@ using UnityEngine;
 
 public class Tools {
 
-	public static int world_width;
-	public static int world_height;
-	public static Sprite sprite;
+	public static PrefabType tile;
 
 	[MenuItem("Tools/CreateTileBoard")]
 	static void Create() {
-		for (int x = 0; x < world_width; x++) {
-			for (int y = 0; y < world_height; y++) {
-				GameObject tile_object = new GameObject ();
-				tile_object.name = "Tile_" + x + "_" + y;
-				tile_object.transform.position = new Vector3 (x, y, 0);
-				SpriteRenderer tile_sprite = tile_object.AddComponent<SpriteRenderer> ();
-				tile_sprite.sprite = sprite;
-			}
+
+
+
 		}
 	}
 }
