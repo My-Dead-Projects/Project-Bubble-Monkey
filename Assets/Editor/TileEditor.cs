7b47af8c9265d96cf9b72098a698c59c1c23a86d
Michael Dorst <code@mikefdorst.com>
2017-01-03 04:19:41 -0800

    Add Tile material switcher to editor (not working)
    The part where we set the material is causing problems somehow.

--- Assets/Editor/TileEditor.cs
+++ Assets/Editor/TileEditor.cs
@@ -1,14 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
+using UnityEngine;
 
 [CustomEditor(typeof(Tile))]
 public class TileEditor : Editor {
 
+	enum Type {
+		Blank,
+		Portal
+	}
+
+	// Represents the type of tile this is
+	Type type;
+
 	public override void OnInspectorGUI() {
 		Tile tile = (Tile)target;
 
-		EditorGUILayout.IntField (tile.x);
-		EditorGUILayout.IntField (tile.y);
+		// Create a dropdown list of tile types
+		type = (Type)EditorGUILayout.EnumPopup ("Type", type);
+
+		switch (type) {
+		case Type.Blank:
+			tile.GetComponent<Renderer> ().material = Resources.Load ("Materials/Blank Tile", typeof(Material)) as Material;
+			break;
+		case Type.Portal:
+			tile.GetComponent<Renderer> ().material = Resources.Load("Materials/Portal Tile", typeof(Material)) as Material;
+			break;
+		}
 	}
 }
