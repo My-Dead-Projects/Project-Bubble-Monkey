5777f4226cafc294649393d336bad657363be9aa
Michael Dorst <code@mikefdorst.com>
2017-01-03 21:30:29 -0800

    Bug fix: Store tile type information on tiles
    I was storing the tile type information on the tile editor, which was
    causing some weird behavior when trying to select different types.
    Obviously this attribute belongs to the Tile class, not the TileEditor
    class.

--- Assets/Editor/TileEditor.cs
+++ Assets/Editor/TileEditor.cs
@@ -6,25 +6,20 @@ using UnityEngine;
 [CustomEditor(typeof(Tile))]
 public class TileEditor : Editor {
 
-	enum Type {
-		Blank,
-		Portal
-	}
-
-	// Represents the type of tile this is
-	Type type;
-
 	public override void OnInspectorGUI() {
 		Tile tile = (Tile)target;
 
-		// Create a dropdown list of tile types
-		type = (Type)EditorGUILayout.EnumPopup ("Type", type);
+		tile.x = EditorGUILayout.IntField ("X", tile.x);
+		tile.y = EditorGUILayout.IntField ("Y", tile.y);
 
-		switch (type) {
-		case Type.Blank:
+		// Create a dropdown list of tile types
+		tile.type = (Tile.Type)EditorGUILayout.EnumPopup ("Type", tile.type);
+
+		switch (tile.type) {
+		case Tile.Type.Blank:
 			tile.GetComponent<Renderer> ().material = Resources.Load ("Materials/Blank Tile", typeof(Material)) as Material;
 			break;
-		case Type.Portal:
+		case Tile.Type.Portal:
 			tile.GetComponent<Renderer> ().material = Resources.Load("Materials/Portal Tile", typeof(Material)) as Material;
 			break;
 		}
--- Assets/Script/Model/Tile.cs
+++ Assets/Script/Model/Tile.cs
@@ -2,6 +2,15 @@
 using UnityEngine;
 
 public class Tile : MonoBehaviour {
+
 	public int x;
 	public int y;
+
+	public enum Type {
+		Blank,
+		Portal
+	}
+
+	// Represents the type of tile this is
+	public Type type;
 }
